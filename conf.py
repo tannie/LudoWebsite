9fc218d926842033727a9f37cbc3b2fee73f31e1
Tanja <GitHub@tannie.nl>
2022-09-06 10:33:41 +0200

    updated email

--- conf.py
+++ conf.py
@@ -24,7 +24,7 @@ SITE_URL = "https://ludo.events/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://ludo.events/"
-BLOG_EMAIL = "tanjaorme@gmail.com"
+BLOG_EMAIL = ""
 BLOG_DESCRIPTION = "Let's play!"  # (translatable)
 
 # Nikola is multilingual!
